bc8b470c6b4fe6875fb820da9f408b967441e661
joelectronics <righteouslyalgebraic@gmail.com>
2016-01-29 21:26:03 +1300

    Added input for 4 controllers
    Use strings:
    Horizontal_P1
    Vertical_P1
    Interact_P1
    
    for each character. Swap P1 for whatever player (P1, P2, P3, P4)

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [Header("Input Axis Names")]
+    [SerializeField] string HorizontalAxis;
+    [SerializeField] string VerticalAxis;
+    [SerializeField] string InteractButtonAxis;
+
+    [Header("Movement Parameters")]
+    public float MoveSpeed = 1f;
+
+	void Start ()
+    {       
+        characterController = GetComponent<CharacterController>();	
+	}	
+
+	void Update ()
+    {
+        //Move player when horizontal or vertical input is given
+        Vector2 input = GetInput();
+        if (input.magnitude > 0)
+        {
+            characterController.SimpleMove(new Vector3(input.x, 0, input.y) * MoveSpeed);
+        }
+	
+	}
+
+    Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxis(HorizontalAxis), Input.GetAxis(VerticalAxis));
+    }
+
+    void Move()
+    {
+
+    }
+}
