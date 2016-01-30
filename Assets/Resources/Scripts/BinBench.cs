61b9d13169dd667b5a567f4c1fe0a1be5e27f5f4
Declan <declantvb@gmail.com>
2016-01-30 20:26:52 +1300

    bin

---
+++ Assets/Resources/Scripts/BinBench.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class BinBench : BenchBase
+{
+	public override IngredientBase Interact(Player player, Vector2 input)
+    {
+		return null;
+	}
+
+	public override bool CanIReceive(IngredientBase item)
+	{
+		return true;
+	}
+
+	public override void GetItem(IngredientBase item) { }
+}
