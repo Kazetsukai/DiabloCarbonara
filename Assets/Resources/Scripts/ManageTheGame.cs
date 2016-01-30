192c235bccec9479ef0857218720f77a47f7d735
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-01-30 16:05:37 +1300

    Umm

--- Assets/Resources/Scripts/ManageTheGame.cs
+++ Assets/Resources/Scripts/ManageTheGame.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ManageTheGame : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class ManageTheGame : MonoBehaviour {
     public GameObject[] ActivePlayers;
 
     public GameObject PlayerProto;
+
+    public Text StartingText;
 
     void Awake()
     {
@@ -28,14 +31,32 @@ public class ManageTheGame : MonoBehaviour {
             if (Input.GetButtonDown("Interact_P" + pNum))
                 Debug.Log( " " + pNum);
 
-            if (Input.GetButtonDown("Interact_P" + pNum) && ActivePlayers[i] == null)
+            if (Input.GetButtonDown("Interact_P" + pNum))
             {
-                ActivePlayers[i] = (GameObject)Instantiate(PlayerProto, new Vector3(((int)((i + 1) / 2) * 2) * Mathf.Pow(-1, (i + 1)), 1, 0), Quaternion.identity);
-                var player = ActivePlayers[i].GetComponent<Player>();
-                player.HorizontalAxis = "Horizontal_P" + pNum;
-                player.VerticalAxis = "Vertical_P" + pNum;
-                player.InteractButtonAxis = "Interact_P" + pNum;
+                if (ActivePlayers[i] == null)
+                {
+                    ActivePlayers[i] = (GameObject)Instantiate(PlayerProto, new Vector3(((int)((i + 1) / 2) * 2) * Mathf.Pow(-1, (i + 1)), 1, 0), Quaternion.identity);
+                    var player = ActivePlayers[i].GetComponent<Player>();
+                    player.HorizontalAxis = "Horizontal_P" + pNum;
+                    player.VerticalAxis = "Vertical_P" + pNum;
+                    player.InteractButtonAxis = "Interact_P" + pNum;
+                }
+                else
+                {
+                    StartCoroutine(BeginGame());
+                }
             }
         }
 	}
+
+    IEnumerator BeginGame()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            StartingText.text = "Beginning in " + (3 - i) + "...";
+            yield return new WaitForSeconds(1);
+        }
+        
+        
+    }
 }
