0f5a12d3d28897c6ef511a9849966ce6a1ebb0b3
Declan <declantvb@gmail.com>
2016-01-30 13:25:11 +1300

    new plateBench

---
+++ Assets/Resources/Scripts/PlateBench.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class PlateBench : BenchBase
+{
+	public GameObject progressImagePrefab;
+	public GameObject progressImage;
+
+	private Recipe Recipe;
+	private IngredientBase[] Ingredients;
+
+	public override IngredientBase Interact()
+	{
+		return null;
+	}
+
+	public override bool CanIReceive(IngredientBase item)
+	{
+		if (Recipe == null)
+		{
+			return false;
+		}
+
+		if (MatchesRecipe(item))
+		{
+			Ingredients.Add(item);
+			return true;
+		}
+
+		return false;
+	}
+
+	private bool MatchesRecipe(IngredientBase item)
+	{
+		return true;
+		if (Recipe.Ingredients.Any(x => x.Type == item.Type))
+		{
+
+		}
+	}
+
+	public void Start()
+	{
+		Ingredients = new List<IngredientBase>();
+		progressImage = Instantiate(progressImagePrefab);
+		var canvas = FindObjectOfType<Canvas>();
+		progressImage.transform.parent = canvas.transform;
+	}
+
+	public new void Update()
+	{
+		Vector3 pos = transform.position + Vector3.up * 3;
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(pos);
+		progressImage.GetComponent<RectTransform>().position = screenPos;
+		progressImage.GetComponent<Image>().fillAmount = 0;
+
+		base.Update();
+	}
+
+	public void FixedUpdate()
+	{
+
+	}
+}
