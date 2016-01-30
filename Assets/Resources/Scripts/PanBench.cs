4344b0426316d4b20d2b94bd612345da3c5de8c5
Declan <declantvb@gmail.com>
2016-01-31 12:06:07 +1300

    fix merge

--- Assets/Resources/Scripts/PanBench.cs
+++ Assets/Resources/Scripts/PanBench.cs
@@ -1,95 +1,114 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Linq;
 
 public class PanBench : BenchBase
 {
-    [Header("Pan Bench")]
-    public GameObject Spatula;
-    public Transform StirTarget;
-    public float StirRadius = 0.5f;    
-    public float progress;
-    public int StirCount = 10;
-    public float MinimumStirAngleProgress = 15f;        //To stop player skipping stirring full circles
-    
+	[Header("Pan Bench")]
+	public GameObject Spatula;
+	public Transform StirTarget;
+	public float StirRadius = 0.5f;
+	public float progress;
+	public float burning;
+	public float burningCooldown;
+	private const float burnTime = 5f;
+	public int StirCount = 10;
+	public float MinimumStirAngleProgress = 15f;        //To stop player skipping stirring full circles
+
 	public GameObject progressImagePrefab;
 	public GameObject progressImage;
 
 	public string TaskType = "Fry";
 
-    public float StirAngle;
-    public float StirAngleProgress;
+	public float StirAngle;
+	public float StirAngleProgress;
 
 
-    Vector3 SpatulaIdlePosition;
-    Vector3 SpatulaIdleRotation;
-    float InteractSpatulaResetTime = 0.1f;
-    float InteractSpatulaResetElapsed;
+	Vector3 SpatulaIdlePosition;
+	Vector3 SpatulaIdleRotation;
+	float InteractSpatulaResetTime = 0.1f;
+	float InteractSpatulaResetElapsed;
 
-    float lastStirAngle;
+	float lastStirAngle;
 
 	private ParticleSystem.EmissionModule Emitter;
+	private ParticleSystem.EmissionModule BurnEmitter;
+	private ParticleSystem.EmissionModule FireEmitter;
+
+	private float FLASH_SCALE = 15f;
+	private float burnThreshold = 1f;
+	private bool burnt;
 
 	public override IngredientBase Interact(Player player, Vector2 input)
-    {
+	{
 		if (contents == null)
 		{
 			return null;
 		}
 
-        LastInteractedPlayer = player;
+		LastInteractedPlayer = player;
 
-        //Get stir angle from input
-        StirAngle = Vector2.Angle(Vector2.up, input);
-        Vector3 cross = Vector3.Cross(Vector2.up, input);
-        if (cross.z > 0)
-        {
-            StirAngle = 360 - StirAngle;
-        }
-
-        //Update stir angle progress (only allow increases by counter clockwise stirring)
-        float deltaStirAngle = Mathf.Clamp(StirAngle - lastStirAngle, 0, 360);
-
-        if (deltaStirAngle > MinimumStirAngleProgress)  //Prevent cheating by skipping stirring in full circles
-        {
-            StirAngleProgress = 0;
-        }
-        else
-        {
-            StirAngleProgress += deltaStirAngle;
-        }
-
-        if (StirAngleProgress >= 360f)
-        {
-            StirAngleProgress = 0f;
-            progress += 1f / StirCount;
-        }
-        
-        lastStirAngle = StirAngle;
-
-        //Do animation for players arms
-        player.IKArm_R.solver.target = HandIKTarget_R;          //Set IK target R of player to be IK transform R of this bench                                                    
-        player.IKArm_L.solver.target = HandIKTarget_L;          //Set IK target L of player to be IK transform L of this bench
-
-        HandIKTarget_L.transform.position = StirTarget.position;
-
-        //Move stir target for spatula
-        StirTarget.transform.localPosition = new Vector3(StirRadius * Mathf.Cos((-StirAngle + 180) * Mathf.Deg2Rad), StirTarget.transform.position.y, StirRadius * Mathf.Sin((-StirAngle + 180) * Mathf.Deg2Rad));
-
-        //Move spatula accordingly
-        InteractSpatulaResetElapsed = 0;
-        Spatula.transform.position = player.HandLeft.transform.position;
-        Spatula.transform.LookAt(StirTarget.position);              
-
-        if (progress >= 1)
+		//Get stir angle from input
+		StirAngle = Vector2.Angle(Vector2.up, input);
+		Vector3 cross = Vector3.Cross(Vector2.up, input);
+		if (cross.z > 0)
 		{
-            contents.Process(TaskType);
+			StirAngle = 360 - StirAngle;
+		}
 
-            //Reset player arm IK targets
-            player.IKArm_R.solver.target = player.ArmIKTarget_R;
-            player.IKArm_L.solver.target = player.ArmIKTarget_L;
+		//Update stir angle progress (only allow increases by counter clockwise stirring)
+		float deltaStirAngle = Mathf.Clamp(StirAngle - lastStirAngle, 0, 360);
 
-            progress = 0;
+		if (deltaStirAngle > MinimumStirAngleProgress)  //Prevent cheating by skipping stirring in full circles
+		{
+			StirAngleProgress = 0;
+		}
+		else
+		{
+			StirAngleProgress += deltaStirAngle;
+		}
+
+		if (StirAngleProgress >= 360f)
+		{
+			StirAngleProgress = 0f;
+			progress += 1f / StirCount;
+			burningCooldown = Time.time + 5f;
+			burning = 0;
+		}
+
+		lastStirAngle = StirAngle;
+
+		//Do animation for players arms
+		player.IKArm_R.solver.target = HandIKTarget_R;          //Set IK target R of player to be IK transform R of this bench                                                    
+		player.IKArm_L.solver.target = HandIKTarget_L;          //Set IK target L of player to be IK transform L of this bench
+
+		HandIKTarget_L.transform.position = StirTarget.position;
+
+		//Move stir target for spatula
+		StirTarget.transform.localPosition = new Vector3(StirRadius * Mathf.Cos((-StirAngle + 180) * Mathf.Deg2Rad), StirTarget.transform.position.y, StirRadius * Mathf.Sin((-StirAngle + 180) * Mathf.Deg2Rad));
+
+		//Move spatula accordingly
+		InteractSpatulaResetElapsed = 0;
+		Spatula.transform.position = player.HandLeft.transform.position;
+		Spatula.transform.LookAt(StirTarget.position);
+
+		if (progress >= 1)
+		{
+			if (burnt)
+			{
+				contents.Burn();
+			}
+			else
+			{
+				contents.Process(TaskType);
+			}
+
+			//Reset player arm IK targets
+			player.IKArm_R.solver.target = player.ArmIKTarget_R;
+			player.IKArm_L.solver.target = player.ArmIKTarget_L;
+
+			progress = 0;
 			var temp = contents;
 			contents = null;
 			return temp;
@@ -102,19 +121,27 @@ public class PanBench : BenchBase
 
 	public override bool CanIReceive(IngredientBase item)
 	{
+		burningCooldown = Time.time + 5f;
 		return true;
 	}
 
 	public void Start()
 	{
-		Emitter = GetComponentInChildren<ParticleSystem>().emission;
+		var particleSystems = GetComponentsInChildren<ParticleSystem>();
+		var psDict = particleSystems.ToDictionary(ps => ps.gameObject.name, ps => ps.emission);
+
+		if (psDict.ContainsKey("particles")) Emitter = psDict["particles"];
+		if (psDict.ContainsKey("burning")) BurnEmitter = psDict["burning"];
+		if (psDict.ContainsKey("fire")) FireEmitter = psDict["fire"];
+
 		progressImage = Instantiate(progressImagePrefab);
+		progressImage.GetComponent<Image>().color = Color.blue;
 		var canvas = FindObjectOfType<Canvas>();
 		progressImage.transform.SetParent(canvas.transform);
 
-        SpatulaIdlePosition = Spatula.transform.position;
-        SpatulaIdleRotation = Spatula.transform.eulerAngles;
-    }
+		SpatulaIdlePosition = Spatula.transform.position;
+		SpatulaIdleRotation = Spatula.transform.eulerAngles;
+	}
 
 	public new void Update()
 	{
@@ -123,31 +150,73 @@ public class PanBench : BenchBase
 		progressImage.GetComponent<RectTransform>().position = screenPos;
 		progressImage.GetComponent<Image>().fillAmount = progress;
 
-        //Update Spatula position
-        InteractSpatulaResetElapsed += Time.deltaTime;
-        if (InteractSpatulaResetElapsed >= InteractSpatulaResetTime)
-        {
-            Spatula.transform.position = SpatulaIdlePosition;
-            Spatula.transform.eulerAngles = SpatulaIdleRotation;
+		//Update Spatula position
+		InteractSpatulaResetElapsed += Time.deltaTime;
+		if (InteractSpatulaResetElapsed >= InteractSpatulaResetTime)
+		{
+			Spatula.transform.position = SpatulaIdlePosition;
+			Spatula.transform.eulerAngles = SpatulaIdleRotation;
 
-            //Reset player arm IK targets
-            if (LastInteractedPlayer != null)
-            {
-                LastInteractedPlayer.IKArm_R.solver.target = LastInteractedPlayer.ArmIKTarget_R;
-                LastInteractedPlayer.IKArm_L.solver.target = LastInteractedPlayer.ArmIKTarget_L;
-                LastInteractedPlayer = null;
-            }
-        }
+			//Reset player arm IK targets
+			if (LastInteractedPlayer != null)
+			{
+				LastInteractedPlayer.IKArm_R.solver.target = LastInteractedPlayer.ArmIKTarget_R;
+				LastInteractedPlayer.IKArm_L.solver.target = LastInteractedPlayer.ArmIKTarget_L;
+				LastInteractedPlayer = null;
+			}
+		}
+
+		BurnEmitter.enabled = false;
+		FireEmitter.enabled = false;
+		if (contents != null)
+		{
+			Color newColor;
+
+			//burning
+			if (burning > 0)
+			{
+				if (burning > burnThreshold)
+				{
+					burnt = true;
+					progress = 1;
+					newColor = Color.black;
+					FireEmitter.enabled = true;
+				}
+				else
+				{
+					newColor = Color.red * ((Mathf.Sin(Time.time * FLASH_SCALE) / 2) + 0.5f);
+					BurnEmitter.enabled = true;
+				}
+			}
+			else
+			{
+				newColor = Color.blue;
+			}
+
+			newColor.a = 0.8f;
+			progressImage.GetComponent<Image>().color = newColor;
+		}
 
 		if (!Emitter.enabled && contents != null)
 		{
-            Emitter.enabled = true;
+			Emitter.enabled = true;
 		}
 		else if (Emitter.enabled && contents == null)
 		{
 			Emitter.enabled = false;
 		}
 
-        base.Update();
+		base.Update();
+	}
+
+	public void FixedUpdate()
+	{
+		if (contents != null)
+		{
+			if (Time.time > burningCooldown)
+			{
+				burning += (1f / burnTime) * Time.fixedDeltaTime;
+			}
+		}
 	}
 }
